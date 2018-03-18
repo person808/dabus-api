18038713e08653517023232d631f7f8e803b33bd
person808 <kainaluh808@gmail.com>
2018-03-17 21:57:10 -0700

    Unconditionally update gtfs data in db_upgrade.py script.

--- README.md
+++ README.md
@@ -7,6 +7,4 @@ A simple API wrapper around the DaBus API and DaBus's GTFS data.
 ## Running the server
 
 A DaBus API token is needed. It is read from the `DABUS_API_KEY` environment
-variable. The `db_upgrade.py` script requires a
-[Transitfeeds](http://transitfeeds.com/) API key to be set at the
-`TRANSITFEEDS_API_KEY` environment variable.
+variable.
--- db_upgrade.py
+++ db_upgrade.py
@@ -1,43 +1,19 @@
-import json
 import os
 import requests
 import subprocess
-from datetime import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Transitfeed api key
-API_KEY = os.environ.get('TRANSITFEEDS_API_KEY')
-FEED_ID = 'thebus-honolulu/57'
 DATABASE_URI = os.environ.get('DATABASE_URL') or os.path.join(
     basedir, 'gtfs.sqlite')
+FEED_URL = 'http://webapps.thebus.org/transitdata/Production/google_transit.zip'
 
-# URL parametes
-parameters = {'key': API_KEY, 'feed': FEED_ID}
+response = requests.get(FEED_URL, stream=True)
+with open('gtfs.zip', mode='wb') as f:
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:  # filter out keep-alive new chunks
+            f.write(chunk)
 
-
-def new_version_available():
-    """Returns True if a new version of the gtfs data is available."""
-    response = requests.get(
-        'https://api.transitfeeds.com/v1/getFeedVersions', params=parameters)
-    versions = json.loads(response.text)
-    # Get the start date of the latest feed
-    latest_version = versions['results']['versions'][0]['d']['s']
-    if latest_version == datetime.now().strftime('%Y%m%d'):
-        return True
-    return False
-
-
-if not os.path.isfile(DATABASE_URI) or new_version_available():
-    # Download new gtfs zip
-    response = requests.get(
-        'https://api.transitfeeds.com/v1/getLatestFeedVersion',
-        params=parameters,
-        stream=True)
-    with open('gtfs.zip', mode='wb') as f:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-    subprocess.run(['gtfs2db', 'overwrite', 'gtfs.zip', DATABASE_URI])
-    os.remove('gtfs.zip')
+subprocess.run(['gtfs2db', 'overwrite', 'gtfs.zip', DATABASE_URI])
+os.remove('gtfs.zip')
