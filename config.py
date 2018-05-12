fec22ff64b2e10b6fff6a8902ace75129077fe29
person808 <kainaluh808@gmail.com>
2018-05-11 22:23:31 -0700

    Disable thread checking in dao.
    Flask is now threaded by default, but I don't have control over how the Dao is created, so disable this check.

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 API_KEY = os.environ.get('DABUS_API_KEY')
 DATABASE_URI = os.environ.get('DATABASE_URL') or os.path.join(basedir, 'gtfs.sqlite')
 FEED_ID = 'hnl'
-dao = Dao(DATABASE_URI)
+dao = Dao('sqlite:///{}?check_same_thread=False'.format(DATABASE_URI))
